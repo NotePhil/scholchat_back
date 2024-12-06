c288ed8a33581379570aa9ec084eab7c53092f09
guyprince <kemogne.prince@cameroonit.cm>
2024-12-06 08:10:23 +0100

    mis a jour

--- business/src/main/resources/data-h2.sql
+++ business/src/main/resources/data-h2.sql
@@ -21,3 +21,9 @@ INSERT INTO ressources.recevoir (message_id, utilisateur_id) VALUES
 ('550e8400-e29b-41d4-a716-446655440012', '550e8400-e29b-41d4-a716-446655440003'),
 ('550e8400-e29b-41d4-a716-446655440012', '550e8400-e29b-41d4-a716-446655440002'),
 ('550e8400-e29b-41d4-a716-446655440013', '550e8400-e29b-41d4-a716-446655440002');
+INSERT INTO ressources.professeurs (id, cniUrlFront, cniUrlBack, nomEtablissement, nomClasse, matriculeProfesseur) VALUES
+('550e8400-e29b-41d4-a716-446655440000', 'path_to_cni_front_1', 'path_to_cni_back_1', 'Université de Douala', 'Classe de Mathématiques', 'MATH101'),
+('550e8400-e29b-41d4-a716-446655440001', 'path_to_cni_front_2', 'path_to_cni_back_2', 'École Normale Supérieure', 'Classe de Physique', 'PHYS102');
+INSERT INTO ressources.repetiteurs (id, cniUrlFront, cniUrlBack, pieceIdentite, photo) VALUES
+('550e8400-e29b-41d4-a716-446655440002', 'path_to_cni_front_3', 'path_to_cni_back_3', 'CNI_ID_3', 'photo_url_3'),
+('550e8400-e29b-41d4-a716-446655440003', 'path_to_cni_front_4', 'path_to_cni_back_4', 'CNI_ID_4', 'photo_url_4');
--- business/src/main/resources/schema-h2.sql
+++ business/src/main/resources/schema-h2.sql
@@ -1,6 +1,16 @@
-DROP DATABASE IF EXISTS parcours;
-
+-- DROP DATABASE IF EXISTS parcours;
 CREATE SCHEMA IF NOT EXISTS ressources AUTHORIZATION sa;
+CREATE TABLE IF NOT EXISTS ressources.messages
+(
+    id               VARCHAR(255) NOT NULL,
+    contenu          VARCHAR(255),
+    datecreation     VARCHAR(255),
+    datemodification VARCHAR(255),
+    etat             VARCHAR(255),
+    expediteur_id    VARCHAR(255),
+    CONSTRAINT pk_messages PRIMARY KEY (id)
+);
+
 
 CREATE TABLE IF NOT EXISTS ressources.utilisateurs
 (
@@ -16,45 +26,38 @@ CREATE TABLE IF NOT EXISTS ressources.utilisateurs
 );
 CREATE TABLE IF NOT EXISTS ressources.professeurs
 (
-    id                     VARCHAR(255) NOT NULL,
-    cni_url_front          VARCHAR(255),
-    cni_url_back           VARCHAR(255),
-    nom_etablissement      VARCHAR(255),
-    nom_classe             VARCHAR(255),
-    matricule_professeur   VARCHAR(255) UNIQUE,
+    id                  VARCHAR(255) NOT NULL,
+    cniUrlFront         VARCHAR(255),
+    cniUrlBack          VARCHAR(255),
+    nomEtablissement    VARCHAR(255),
+    nomClasse           VARCHAR(255),
+    matriculeProfesseur VARCHAR(255),
     CONSTRAINT pk_professeurs PRIMARY KEY (id),
-    CONSTRAINT fk_professeurs_on_utilisateurs FOREIGN KEY (id) REFERENCES ressources.utilisateurs (id)
+    CONSTRAINT fk_professeurs_utilisateurs FOREIGN KEY (id) REFERENCES ressources.utilisateurs (id)
 );
+
 CREATE TABLE IF NOT EXISTS ressources.repetiteurs
 (
     id                VARCHAR(255) NOT NULL,
-    cni_url_front     VARCHAR(255),
-    cni_url_back      VARCHAR(255),
-    piece_identite    VARCHAR(255),
+    cniUrlFront       VARCHAR(255),
+    cniUrlBack        VARCHAR(255),
+    pieceIdentite     VARCHAR(255),
     photo             VARCHAR(255),
     CONSTRAINT pk_repetiteurs PRIMARY KEY (id),
-    CONSTRAINT fk_repetiteurs_on_utilisateurs FOREIGN KEY (id) REFERENCES ressources.utilisateurs (id)
+    CONSTRAINT fk_repetiteurs_utilisateurs FOREIGN KEY (id) REFERENCES ressources.utilisateurs (id)
 );
 
--- Table for Messages
-CREATE TABLE IF NOT EXISTS ressources.messages
-(
-    id               VARCHAR(255) NOT NULL,
-    contenu          VARCHAR(255),
-    datecreation     VARCHAR(255),
-    datemodification VARCHAR(255),
-    etat             VARCHAR(255),
-    expediteur_id    VARCHAR(255),
-    CONSTRAINT pk_messages PRIMARY KEY (id),
-    CONSTRAINT fk_messages_on_expediteur FOREIGN KEY (expediteur_id) REFERENCES ressources.utilisateurs (id)
-);
-
--- Table for Recevoir
 CREATE TABLE IF NOT EXISTS ressources.recevoir
 (
     message_id     VARCHAR(255) NOT NULL,
-    utilisateur_id VARCHAR(255) NOT NULL,
-    CONSTRAINT pk_recevoir PRIMARY KEY (message_id, utilisateur_id),
-    CONSTRAINT fk_recevoir_on_messages FOREIGN KEY (message_id) REFERENCES ressources.messages (id),
-    CONSTRAINT fk_recevoir_on_utilisateurs FOREIGN KEY (utilisateur_id) REFERENCES ressources.utilisateurs (id)
+    utilisateur_id VARCHAR(255) NOT NULL
 );
+
+ALTER TABLE  ressources.recevoir
+    ADD CONSTRAINT IF NOT EXISTS  fk_recevoir_on_messages_entity FOREIGN KEY (message_id) REFERENCES ressources.messages (id);
+
+ALTER TABLE ressources.recevoir
+    ADD CONSTRAINT IF NOT EXISTS  fk_recevoir_on_utilisateurs_entity FOREIGN KEY (utilisateur_id) REFERENCES ressources.utilisateurs (id);
+
+ALTER TABLE ressources.messages
+    ADD CONSTRAINT IF NOT EXISTS  FK_MESSAGES_ON_EXPEDITEUR FOREIGN KEY (expediteur_id) REFERENCES ressources.utilisateurs (id);
