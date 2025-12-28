b628a8d31b5fad9f056e9c5b6df1d6f555a22ddb
Notephil <ngongang_philippe@yahoo.fr>
2025-12-28 02:32:19 +0100

    test sorti propre du projet
    a la fin du déploiement on souhaite avoir un status ok

--- db-init/src/main/java/cmr/notep/dbinit/DbInitApplication.java
+++ db-init/src/main/java/cmr/notep/dbinit/DbInitApplication.java
@@ -1,14 +1,21 @@
 package cmr.notep.dbinit;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+@Slf4j
 @SpringBootApplication
 public class DbInitApplication {
 
     public static void main(String[] args) {
         // Single instance semantics are handled at IaC / deployment level (one-off job)
-        SpringApplication.run(DbInitApplication.class, args);
+       // SpringApplication.run(DbInitApplication.class, args);
+        System.exit(SpringApplication.exit(SpringApplication.run(DbInitApplication.class, args)));
+    }
+
+    public void run(String... args) throws Exception {
+        log.info("Database initialization completed successfully");
+        // L'application se fermera automatiquement après ce point
     }
 }
 
