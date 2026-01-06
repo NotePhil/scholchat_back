74b146e4c37626bc1fe5bf5170e2f208556570ff
Notephil <ngongang_philippe@yahoo.fr>
2026-01-07 00:44:57 +0100

    variabilisation de l'url du front

--- business/src/main/java/cmr/notep/business/config/CorsConfig.java
+++ business/src/main/java/cmr/notep/business/config/CorsConfig.java
@@ -1,5 +1,6 @@
 package cmr.notep.business.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
@@ -7,6 +8,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class CorsConfig {
+    @Value("${front.endpoint}")
+    private String frontEndpoint;
 
     @Bean
     public WebMvcConfigurer corsConfigurer() {
@@ -14,7 +17,7 @@ public class CorsConfig {
             @Override
             public void addCorsMappings(CorsRegistry registry) {
                 registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000", "http://localhost:9000")
+                        .allowedOrigins(frontEndpoint)
                         .allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
                         .allowCredentials(true)
