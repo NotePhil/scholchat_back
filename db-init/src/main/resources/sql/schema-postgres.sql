fbec4783b5df2c1285c06c9a252bd4a8af724901
Notephil <ngongang_philippe@yahoo.fr>
2026-01-21 00:52:48 +0100

    unicité email dans le bon projet

--- db-init/src/main/resources/sql/schema-postgres.sql
+++ db-init/src/main/resources/sql/schema-postgres.sql
@@ -6,7 +6,7 @@ CREATE TABLE IF NOT EXISTS utilisateurs (
     id VARCHAR(255) NOT NULL,
     nom VARCHAR(255),
     prenom VARCHAR(255),
-    email VARCHAR(255),
+    email VARCHAR(255) UNIQUE,
     passeaccess VARCHAR(255),
     telephone VARCHAR(255),
     adresse VARCHAR(255),
