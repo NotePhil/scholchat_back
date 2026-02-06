5198720e2751440c53b99ef62264b9dbf2ef89d9
guyprince <kemogne.prince@cameroonit.cm>
2026-02-06 14:24:37 +0100

    Add CommentRequest DTO for event comments

---
+++ interfaces/src/main/java/cmr/notep/interfaces/modeles/CommentRequest.java
@@ -0,0 +1,12 @@
+package cmr.notep.interfaces.modeles;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class CommentRequest {
+    private String content;
+}
